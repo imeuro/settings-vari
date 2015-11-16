2611f63f542aeacb1e4f15042b889bfe003338c5
Mauro Fioravanzi <imeuro@users.noreply.github.com>
2015-11-16 17:47:07 +0100

    Create torrent-done.sh
    with opensubtitles.py

---
+++ torrent-done.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# find new torrent and look for avail subtitles
+torrent_path="${TR_TORRENT_DIR}/${TR_TORRENT_NAME}"
+NOW=$(date +%Y-%m-%d\ %H:%M:%S)
+
+if [[ ! -z $torrent_path ]]
+then
+  echo "$NOW : cerco sottotitoli per $TR_TORRENT_NAME ( $torrent_path )" >> /torrents/log.txt
+  python3 /torrents/opensubtitles.py $TR_TORRENT_DIR/$TR_TORRENT_NAME;
+else
+  #python3 /torrents/opensubtitles.py /torrents/Downloads/The.Last.Man.On.Earth.S02E07.HDTV.x264-KILLERS\[rarbg\]/
+  #echo "$NOW : just testing with /torrents/Downloads/The.Last.Man.On.Earth.S02E07.HDTV.x264-KILLERS[rarbg]" >> /torrents/log.txt
+  echo "$NOW : $torrent_path vuoto!" >> /torrents/log.txt
+fi
+
+# on torrent complete, take ownership of the folders
+chown -R io:users /torrents/Downloads/ &&
+chmod -R 755 /torrents/Downloads/ &&
+
+exit 0
